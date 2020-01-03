b0326f4335d7a92fcfdd3bccadccb54941dc7b1e
benetherington <ben.j.etherington@gmail.com>
2020-01-02 19:39:15 -0800

    Give users an access token and a subscribed bool. Update validations to not require passwords for non-admins.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ApplicationRecord
-  has_secure_password
+
+  has_secure_token :access_token
 
   validates :email,
                     presence: true,
                     uniqueness: true
   validates :admin,
                     inclusion: { in: [true, false] }
+
+  PASSWORD_FORMAT = /\A
+    (?=.{8,})          # Must contain 8 or more characters
+    (?=.*\d)           # Must contain a digit
+    (?=.*[a-z])        # Must contain a lower case character
+    (?=.*[A-Z])        # Must contain an upper case character
+    (?=.*[[:^alnum:]]) # Must contain a symbol
+  /x
+
+  validates :password, 
+                      presence: true,  
+                      format: { with: PASSWORD_FORMAT },
+                      if: -> { self.admin? }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_184833) do
+ActiveRecord::Schema.define(version: 2020_01_03_032839) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 2020_01_02_184833) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "subscribed"
+    t.string "access_token"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
