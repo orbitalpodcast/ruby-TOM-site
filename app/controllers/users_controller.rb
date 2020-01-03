1cb7994730f62289999e5cee247a01510b42f22f
benetherington <ben.j.etherington@gmail.com>
2020-01-02 19:39:59 -0800

    Fix permitted admin bool

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:password_digest, :admin, :email)
+      params.require(:user).permit(:password_digest, :email)
     end
 end
