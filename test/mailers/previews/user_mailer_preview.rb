c36771aea30f2873f515b897b7cfefa30518108d
benetherington <ben.j.etherington@gmail.com>
2020-01-04 10:05:36 -0800

    Begin work on email previewer

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,8 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  def welcome_email
+    @user = User.all_subscribed.first
+    UserMailer.with(user: @user).welcome_email
+  end
+end
