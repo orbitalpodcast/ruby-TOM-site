0a33aed440b1f990660765f23a994240e6ef9dd5
benetherington <ben.j.etherington@gmail.com>
2020-01-09 19:51:55 -0800

    Install delayed_job gem

--- Gemfile
+++ Gemfile
@@ -19,8 +19,13 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
-# Use Active Model has_secure_password
+
+# TODO: consider using protected_attributes gem
+
+# Activate Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+# Use DJ as a queuing backend
+gem 'delayed_job_active_record', '~> 4.1.4'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_03_032839) do
+ActiveRecord::Schema.define(version: 2020_01_06_024747) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,21 @@ ActiveRecord::Schema.define(version: 2020_01_03_032839) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "episodes", force: :cascade do |t|
