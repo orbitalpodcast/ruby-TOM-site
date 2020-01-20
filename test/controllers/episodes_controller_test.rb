ed7a7feebce79f5e99e1d16a1203b09f5c0add80
benetherington <ben.j.etherington@gmail.com>
2020-01-19 18:05:53 -0800

    Write some convert_markup_to_HTML tests

--- test/controllers/episodes_controller_test.rb
+++ test/controllers/episodes_controller_test.rb
@@ -5,10 +5,91 @@ class EpisodesControllerTest < ActionDispatch::IntegrationTest
     @episode = episodes(:one)
   end
 
-  test "should get index" do
-    get episodes_url
-    assert_response :success
+  test "convert_markup_to_html format URLs, accounting for twitter usernames" do
+    @string = ["Spaceflight News",
+               "Starliner OFT https://www.nasaspaceflight.com/2019/12/starliner-mission-shortening-failure-successful-launch/ http://www.collectspace.com/news/news-122219a-boeing-starliner-oft-landing.html",
+               "IR video https://twitter.com/NASA/status/1208735543657320448"].join("\n")
+    @output = ["Spaceflight News",
+               "Starliner OFT (<a href=\"https://www.nasaspaceflight.com/2019/12/starliner-mission-shortening-failure-successful-launch/\">nasaspaceflight.com</a>) (<a href=\"http://www.collectspace.com/news/news-122219a-boeing-starliner-oft-landing.html\">collectspace.com</a>)",
+               "IR video (<a href=\"https://twitter.com/NASA/status/1208735543657320448\">twitter.com/NASA</a>)"].join("\n")
+    assert_equal @output, Episode.convert_markup_to_HTML(@string)
   end
+
+  test "convert_markup_to_html format URLs, accounting for subreddits and forums" do
+    @string = ["Spaceflight News",
+               "* Starship overpressure explosion https://spacenews.com/spacex-starship-suffers-testing-setback/",
+               "** Rumors on 4Chan, via Reddit and NSF forum https://www.reddit.com/r/SpaceXLounge/comments/e055ej/i_can_partially_end_the_speculation_about_why/ https://forum.nasaspaceflight.com/index.php?topic=49114.1540"].join("\n")
+    @output = ["Spaceflight News",
+               "* Starship overpressure explosion (<a href=\"https://spacenews.com/spacex-starship-suffers-testing-setback/\">spacenews.com</a>)",
+               "** Rumors on 4Chan, via Reddit and NSF forum (<a href=\"https://www.reddit.com/r/SpaceXLounge/comments/e055ej/i_can_partially_end_the_speculation_about_why/\">/r/SpaceXLounge</a>) (<a href=\"https://forum.nasaspaceflight.com/index.php?topic=49114.1540\">forum.nasaspaceflight.com</a>)"].join("\n")
+    assert_equal @output, Episode.convert_markup_to_HTML(@string)
+  end
+
+  test "convert_markup_to_html format URLs, accounting for file type" do
+    @string = ["This week in SF history",
+               "* 29 October 1998: Launch of STS-95 https://en.wikipedia.org/wiki/STS-95 https://www.nasa.gov/home/hqnews/presskit/1998/sts-95.pdf",
+               "* Next week in 1974: Solidarity"].join("\n")
+    @output = ["This week in SF history",
+               "* 29 October 1998: Launch of STS-95 (<a href=\"https://en.wikipedia.org/wiki/STS-95\">en.wikipedia.org</a>) (PDF: <a href=\"https://www.nasa.gov/home/hqnews/presskit/1998/sts-95.pdf\">nasa.gov</a>)",
+               "* Next week in 1974: Solidarity"].join("\n")
+    assert_equal @output, Episode.convert_markup_to_HTML(@string)
+  end
+
+  # test "convert_markup_to_html format URLs, accounting for existing parens" do
+  #   @string = ["This week in SF history",
+  #              "* 29 October 1998: Launch of STS-95 (https://en.wikipedia.org/wiki/STS-95)",
+  #              "* Next week in 1974: Solidarity"].join("\n")
+  #   @output = ["This week in SF history",
+  #              "* 29 October 1998: Launch of STS-95 (<a href=\"https://en.wikipedia.org/wiki/STS-95\">en.wikipedia.org</a>)",
+  #              "* Next week in 1974: Solidarity"].join("\n")
+  #   assert_equal @output, Episode.convert_markup_to_HTML(@string)
+  # end
+
+  # test "convert_markup_to_html format URLs, accounting for existing parens with padding spaces" do
+  #   @string = ["This week in SF history",
+  #              "* 29 October 1998: Launch of STS-95 (   https://en.wikipedia.org/wiki/STS-95 )",
+  #              "* Next week in 1974: Solidarity"].join("\n")
+  #   @output = ["This week in SF history",
+  #              "* 29 October 1998: Launch of STS-95 (<a href=\"https://en.wikipedia.org/wiki/STS-95\">en.wikipedia.org</a>)",
+  #              "* Next week in 1974: Solidarity"].join("\n")
+  #   assert_equal @output, Episode.convert_markup_to_HTML(@string)
+  # end
+
+  # test "convert_markup_to_html format URLs, accounting for hat tips" do
+    # @string = [""].join("\n")
+    # @output = [""].join("\n")
+    # assert_equal @output, Episode.convert_markup_to_HTML(@string)
+  # end
+
+  # test "convert_markup_to_html format URLs, allowing escaped full URLs" do
+    # @string = [""].join("\n")
+    # @output = [""].join("\n")
+    # assert_equal @output, Episode.convert_markup_to_HTML(@string)
+  # end
+
+  # test "convert_markup_to_html format in-line links" do
+    # @string = [""].join("\n")
+    # @output = [""].join("\n")
+    # assert_equal @output, Episode.convert_markup_to_HTML(@string)
+  # end
+
+  # test "convert_markup_to_html format bold and itallics" do
+    # @string = [""].join("\n")
+    # @output = [""].join("\n")
+    # assert_equal @output, Episode.convert_markup_to_HTML(@string)
+  # end
+
+  # test "convert_markup_to_html format escapement for HTML" do
+    # @string = [""].join("\n")
+    # @output = [""].join("\n")
+    # assert_equal @output, Episode.convert_markup_to_HTML(@string)
+  # end
+
+
+  # test "should get index" do
+  #   get episodes_url
+  #   assert_response :success
+  # end
 
   # test "should get new" do
   #   get root_path
