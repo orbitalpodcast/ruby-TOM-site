fa146aae9e22fbbeabfd62867f644758f47d1def
benetherington <ben.j.etherington@gmail.com>
2020-02-08 13:13:03 -0800

    Add image dimensions to episode edit page.
    Really beginning to regret naming an object image and giving it an attachment called image.

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,12 @@ class Image < ApplicationRecord
   belongs_to :episode
   has_one_attached :image
   scope :with_eager_loaded_images, -> { eager_load(images_attachments: :blob) }
+
+  def get_dimensions
+    if self.dimensions.nil? or self.dimensions.include? nil
+      self.dimensions = [ self.image.metadata[:width], self.image.metadata[:height] ]
+    end
+    self.save if self.changed?
+    self.dimensions
+  end
 end
