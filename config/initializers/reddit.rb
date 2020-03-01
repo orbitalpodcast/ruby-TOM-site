f29610ea71d6337fb7646f071ca00efb488ee531
benetherington <ben.j.etherington@gmail.com>
2020-02-29 17:06:48 -0800

    Switch reddit config to use test subreddit in dev environment

--- config/initializers/reddit.rb
+++ config/initializers/reddit.rb
@@ -1,6 +1,19 @@
 require 'reddit_bot'
 
 # Initialize RedditBot object. Currently disabled in all but development.
+# if ENV["RAILS_ENV"] == 'production'
+#   REDDIT_CLIENT = RedditBot::Bot.new(
+#     user_agent:    'TOM-site:v1.0 (by /u/hapax_legomina)',
+#     client_id:     ENV['REDDIT_CLIENT_ID'],
+#     client_secret: ENV['REDDIT_SECRET'],
+#     login:         ENV['REDDIT_USERNAME'],
+#     password:      ENV['REDDIT_PASSWORD']
+#   )
+#   DEFAULT_REDDIT_PARAMS = {'sr':          'orbitalpodcast',
+#                            'kind':        'link',
+#                            'sendreplies': 'true'}
+# end
+
 if ENV["RAILS_ENV"] == 'development'
   REDDIT_CLIENT = RedditBot::Bot.new(
     user_agent:    'TOM-site:v1.0 (by /u/hapax_legomina)',
@@ -9,7 +22,7 @@ if ENV["RAILS_ENV"] == 'development'
     login:         ENV['REDDIT_USERNAME'],
     password:      ENV['REDDIT_PASSWORD']
   )
-  DEFAULT_REDDIT_PARAMS = {'sr':          'orbitalpodcast',
+  DEFAULT_REDDIT_PARAMS = {'sr':          'tomcast_bottest',
                            'kind':        'link',
                            'sendreplies': 'true'}
 end
