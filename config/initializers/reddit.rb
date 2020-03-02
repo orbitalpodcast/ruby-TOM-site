6fe2228a2bd46cf26a0c74d6959f6ee16628786b
benetherington <ben.j.etherington@gmail.com>
2020-03-01 16:41:58 -0800

    Update reddit initializer to work with tests

--- config/initializers/reddit.rb
+++ config/initializers/reddit.rb
@@ -1,6 +1,21 @@
+# Initialize RedditBot object. Currently disabled in all but development.
 require 'reddit_bot'
 
-# Initialize RedditBot object. Currently disabled in all but development.
+# fake bot for test environment
+class FakeRedditBot
+  def json method, params, form
+    return {"json"=>
+              {"errors"=>[],
+                "data"=>{
+                  "url"=>"https://www.reddit.com/r/tomcast_bottest/comments/fbzc16/episode_241_clock_kerfuffle/",
+                  "drafts_count"=>0,
+                  "id"=>"fbzc16",
+                  "name"=>"t3_fbzc16"}}}
+  end
+end
+
+
+
 # if ENV["RAILS_ENV"] == 'production'
 #   REDDIT_CLIENT = RedditBot::Bot.new(
 #     user_agent:    'TOM-site:v1.0 (by /u/hapax_legomina)',
@@ -25,4 +40,9 @@ if ENV["RAILS_ENV"] == 'development'
   DEFAULT_REDDIT_PARAMS = {'sr':          'tomcast_bottest',
                            'kind':        'link',
                            'sendreplies': 'true'}
+end
+
+if ENV["RAILS_ENV"] == 'test'
+  REDDIT_CLIENT = FakeRedditBot.new()
+  DEFAULT_REDDIT_PARAMS = {}
 end
