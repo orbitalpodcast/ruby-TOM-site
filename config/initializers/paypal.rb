d0d2bf73eeae5c2b5fac2cf003ef892ebd65fe46
benetherington <ben.j.etherington@gmail.com>
2020-04-04 21:00:56 -0700

    Configure paypal

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,2 +1,38 @@
-PayPal::SDK.load("config/paypal.yml", Rails.env)
-PayPal::SDK.logger = Rails.logger
+require 'paypal-checkout-sdk'
+
+module PayPalClient
+  class << self
+
+    # Setting up and Returns PayPal SDK environment with PayPal Access credentials.
+    # For demo purpose, we are using SandboxEnvironment. In production this will be
+    # LiveEnvironment.
+    def environment
+      PayPal::SandboxEnvironment.new(ENV['PAYPAL_CLIENT_ID'],
+                                     ENV['PAYPAL_CLIENT_SECRET'])
+    end
+
+    # Returns PayPal HTTP client instance with environment which has access
+    # credentials context. This can be used invoke PayPal API's provided the
+    # credentials have the access to do so.
+    def client
+      PayPal::PayPalHttpClient.new(self.environment)
+    end
+
+    # Utility to convert Openstruct Object to JSON hash.
+    def openstruct_to_hash(object, hash = {})
+      object.each_pair do |key, value|
+        hash[key] = value.is_a?(OpenStruct) ? openstruct_to_hash(value) : value.is_a?(Array) ? array_to_hash(value) : value
+      end
+      hash
+    end
+
+    # Utility to convert Array of OpenStruct into Hash.
+    def array_to_hash(array, hash= [])
+      array.each do |item|
+        x = item.is_a?(OpenStruct) ? openstruct_to_hash(item) : item.is_a?(Array) ? array_to_hash(item) : item
+        hash << x
+      end
+      hash
+    end
+  end
+end
